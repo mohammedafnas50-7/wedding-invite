fd58e232fd387b2fd630546bdbba66695777967c
Mohammed Anjal Hisam <wedding-invitation@local>
2026-08-12 15:21:55 +0530

    Create wedding invitation website

---
+++ config.js
@@ -0,0 +1,8 @@
+/* Edit the wedding details and asset paths in this file. */
+window.WEDDING = {
+  groom: 'Mohammed Anjal Hisam', bride: 'Fereeha',
+  dateISO: '2026-10-18T11:00:00+05:30', date: '18 October 2026', day: 'Sunday',
+  time: '11:00 AM to 3:00 PM', venue: 'Shifa Convention Centre', city: 'Perinthalmanna',
+  mapUrl: 'https://maps.app.goo.gl/usxpvcMszgMfJReK9?g_st=iw',
+  openingArtwork: 'assets/white-gold-cover.jpg', countdownArtwork: 'assets/moroccan-countdown.png', bridePhoto: 'assets/bride.jpeg', groomPhoto: 'assets/groom.jpeg', music: 'assets/wedding-song.mp3'
+};
