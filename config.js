9b909c3ae5ef65e621a1578a6f342f514042d46b
mohammedafnas50-7 <mohammedafnas50@gmail.com>
2026-09-11 14:53:31 +0530

    Force supplied background music to load

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.WEDDING = {
   dateISO: '2026-10-18T11:00:00+05:30', date: '18 October 2026', day: 'Sunday',
   time: '11:00 AM to 3:00 PM', venue: 'Shifa Convention Centre', city: 'Perinthalmanna',
   mapUrl: 'https://maps.app.goo.gl/usxpvcMszgMfJReK9?g_st=iw',
-  openingArtwork: 'assets/white-gold-cover.jpg', countdownArtwork: 'assets/moroccan-countdown.png', bridePhoto: 'assets/bride.jpeg', groomPhoto: 'assets/groom.jpeg', music: 'assets/haJoyW2MSwA.mp3'
+  openingArtwork: 'assets/white-gold-cover.jpg', countdownArtwork: 'assets/moroccan-countdown.png', bridePhoto: 'assets/bride.jpeg', groomPhoto: 'assets/groom.jpeg', music: 'assets/haJoyW2MSwA.mp3?v=2'
 };
